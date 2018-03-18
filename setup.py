ede65af88062dcf692ef49b84d7e3afda3ebf1f6
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2018-03-18 23:05:27 +0200

    Make classifier more accurate regarding Python 2 support
    Ref: https://docs.python.org/3/howto/pyporting.html#make-sure-you-specify-the-proper-version-support-in-your-setup-py-file

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ Development Status :: 4 - Beta
 License :: OSI Approved :: Apache Software License
 Operating System :: POSIX
 Programming Language :: Python
-Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
 Topic :: Software Development :: Testing
 Framework :: Robot Framework
