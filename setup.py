9d24809f11998efdc453f0e18fe3696bb4b838ad
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2018-03-18 19:10:50 +0200

    Convert README to reStructuredText

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ Framework :: Robot Framework :: Library
 '''.strip().splitlines()
 with open(join(CURDIR, 'src', 'REST', 'version.py')) as f:
     VERSION = re.search("__version__ = '(.*)'", f.read()).group(1)
-with open(join(CURDIR, 'README.md')) as f:
+with open(join(CURDIR, 'README.rst')) as f:
     DESCRIPTION = f.read()
 with open(join(CURDIR, 'requirements.txt')) as f:
     REQUIREMENTS = f.read().splitlines()
