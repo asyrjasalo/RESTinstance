0b25bb81539fabfeb7ca8063dd337a1c39cfe233
ot2789 <32983454+ot2789@users.noreply.github.com>
2021-06-02 11:47:06 +0300

    Http authentication (#8)
    * Added HTTP authentication to the API.
    * Now the requests to HTTP can also contain Basic, Digest and Proxy
    authentication.
    
    * Added auth.robot for testing Basic and Digest authentication.
    
    * Simplified error messages for the Set Client Authentication.
    
    * Added test for invalid arguments. And merged if checks in set client
    auth keyword.
    
    * Fix for the HTTPBasicAuth is not JSON serializable error.
    
    Co-authored-by: Octavian Toader <Octavian.Toader@belden.com>

--- src/REST/keywords.py
+++ src/REST/keywords.py
@@ -24,7 +24,7 @@ from pytz import utc, UnknownTimeZoneError
 from tzlocal import get_localzone
 
 from collections import OrderedDict
-from copy import deepcopy
+from copy import deepcopy, copy
 from datetime import datetime
 from json import dumps, loads
 from os import path, getcwd
@@ -35,6 +35,7 @@ from jsonpath_ng.ext import parse as parse_jsonpath
 from jsonschema import validate, FormatChecker
 from jsonschema.exceptions import SchemaError, ValidationError
 from requests import request as client
+from requests.auth import HTTPDigestAuth, HTTPBasicAuth, HTTPProxyAuth
 from requests.exceptions import SSLError, Timeout
 
 if IS_PYTHON_2:
@@ -76,6 +77,44 @@ class Keywords(object):
         """
         self.request["cert"] = self._input_client_cert(cert)
         return self.request["cert"]
+
+
+    @keyword(name="Set Client Authentication", tags=("settings",))
+    def set_client_authentication(self, auth_type, user=None, password=None):
+        """*Attaches HTTP basic authentication to the given requests.*
+
+        The API by default will not have authentication enabled. 
+
+        The auth_type argument must be ${NONE}, basic, digest or proxy.
+        In case the user sets the auth_type to ${NONE} the authentication 
+        information 
+        The user and password will be written in plain text.
+
+        *Examples*
+
+        | `Set Client Authentication` | basic  | admin | password |
+        | `Set Client Authentication` | digest | admin | password |
+        | `Set Client Authentication` | proxy  | admin | password |
+        | `Set Client Authentication` | ${NONE} | | |
+        """
+        error_auth = "Argument \"auth_type\" must be ${NONE}, basic, digest or proxy."
+        if auth_type != None:
+            if not isinstance(auth_type, str):
+                raise TypeError(error_auth)
+
+            auth_type = auth_type.lower()
+
+            if not auth_type in ["basic", "digest","proxy"] :
+                raise TypeError(error_auth)
+
+            if auth_type == "basic" :
+                auth_type = HTTPBasicAuth
+            elif auth_type == "digest" :
+                auth_type = HTTPDigestAuth
+            elif auth_type == "proxy" :
+                auth_type = HTTPProxyAuth
+
+        return self._setauth(auth_type, user, password)
 
     @keyword(name="Set Headers", tags=("settings",))
     def set_headers(self, headers):
@@ -276,7 +315,7 @@ class Keywords(object):
         | `HEAD` | /users/1 | timeout=0.5 |
         """
         endpoint = self._input_string(endpoint)
-        request = deepcopy(self.request)
+        request = copy(self.request)
         request["method"] = "HEAD"
         if allow_redirects is not None:
             request["allowRedirects"] = self._input_boolean(allow_redirects)
@@ -319,7 +358,7 @@ class Keywords(object):
         | `OPTIONS` | /users/1 | allow_redirects=false |
         """
         endpoint = self._input_string(endpoint)
-        request = deepcopy(self.request)
+        request = copy(self.request)
         request["method"] = "OPTIONS"
         if allow_redirects is not None:
             request["allowRedirects"] = self._input_boolean(allow_redirects)
@@ -375,7 +414,7 @@ class Keywords(object):
         *Data argument is new in version 1.1.0*
         """
         endpoint = self._input_string(endpoint)
-        request = deepcopy(self.request)
+        request = copy(self.request)
         request["method"] = "GET"
         request["query"] = OrderedDict()
         query_in_url = OrderedDict(parse_qsl(urlparse(endpoint).query))
@@ -435,7 +474,7 @@ class Keywords(object):
         *Data argument is new in version 1.1.0*
         """
         endpoint = self._input_string(endpoint)
-        request = deepcopy(self.request)
+        request = copy(self.request)
         request["method"] = "POST"
         request["body"] = self.input(body)
         if allow_redirects is not None:
@@ -489,7 +528,7 @@ class Keywords(object):
         *Data argument is new in version 1.1.0*
         """
         endpoint = self._input_string(endpoint)
-        request = deepcopy(self.request)
+        request = copy(self.request)
         request["method"] = "PUT"
         request["body"] = self.input(body)
         if allow_redirects is not None:
@@ -543,7 +582,7 @@ class Keywords(object):
         *Data argument is new in version 1.1.0*
         """
         endpoint = self._input_string(endpoint)
-        request = deepcopy(self.request)
+        request = copy(self.request)
         request["method"] = "PATCH"
         request["body"] = self.input(body)
         if allow_redirects is not None:
@@ -596,7 +635,7 @@ class Keywords(object):
         *Body argument is new in version 1.1.0*
         """
         endpoint = self._input_string(endpoint)
-        request = deepcopy(self.request)
+        request = copy(self.request)
         request["method"] = "DELETE"
         request["body"]=self.input(body)
         if allow_redirects is not None:
@@ -1293,6 +1332,7 @@ class Keywords(object):
             self.instances,
             ensure_ascii=False,
             indent=4,
+            default=vars,
             separators=(",", ": "),
             sort_keys=sort_keys,
         )
@@ -1326,6 +1366,13 @@ class Keywords(object):
 
     ### Internal methods
 
+    def _setauth(self, auth_type, user = None, password = None):
+        if auth_type == None:
+            self.request["auth"] = None
+        else :
+            self.request["auth"] = auth_type(user, password)
+        return self.request["auth"]
+
     def _request(self, endpoint, request, validate=True):
         if not endpoint.startswith(("http://", "https://")):
             base_url = self.request["scheme"] + "://" + self.request["netloc"]
@@ -1347,6 +1394,7 @@ class Keywords(object):
                 headers=request["headers"],
                 proxies=request["proxies"],
                 cert=request["cert"],
+                auth=request["auth"],
                 timeout=tuple(request["timeout"]),
                 allow_redirects=request["allowRedirects"],
                 verify=request["sslVerify"],
