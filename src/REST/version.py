a698c20ba9d20a2139f50b65115aa8ece7c76a84
Joonas Jauhiainen <joonas.jauhiainen@eficode.com>
2022-01-21 08:15:46 +0200

    Prehook changes

--- src/REST/version.py
+++ src/REST/version.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 #  Copyright 2018-  Anssi Syrjäsalo
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,4 +12,4 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
