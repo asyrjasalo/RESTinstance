c3e504f39b38fcec282fdad6f7fc4e95cac1b784
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2024-07-29 10:23:03 +0300

    build(release): Add publish to PyPIs

--- release.sh
+++ release.sh
@@ -10,18 +10,20 @@ prerelease_type="$1"
 
 branch="$(git branch --show-current)"
 if [ "$branch" != "main" ] && [ "$branch" != "master" ]; then
-  echo "Error: Releases must be created from trunk, run $0 in main/master."
-  exit 1
+    echo "Error: Releases must be created from trunk, run $0 in main/master."
+    exit 1
 fi
 
 if [ -n "$(git status -s)" ]; then
-  echo "Error: Working tree has changes: Stash, commit or reset first"
-  exit 1
+    echo "Error: Working tree has changes: Stash, commit or reset first"
+    exit 1
 fi
 
 if [ -n "$prerelease_type" ]; then
-  echo "Creating pre-release ($prerelease_type)"
-  pipx run --spec commitizen cz bump --prerelease "$prerelease_type"
+    echo "Creating pre-release ($prerelease_type)"
+    pipx run --spec commitizen cz bump --prerelease "$prerelease_type" &&
+    pdm publish --repository testpypi
 else
-  pipx run --spec commitizen cz bump
+    pipx run --spec commitizen cz bump &&
+    pdm publish
 fi
